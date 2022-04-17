7bafbfd3c6a81e8c634ac9994d8b963cadb57a1a
Kenny <27463495+Frontesque@users.noreply.github.com>
2022-04-16 21:14:08 -0400

    Create auto inject example.js

---
+++ auto inject example.js
@@ -0,0 +1,9 @@
+(function(d, s) {
+    let NUXT_Loader = d.createElement('script');
+    NUXT_Loader.type = 'text/javascript';
+    NUXT_Loader.onload = function(){
+        // NUXT is injected here
+    };
+    NUXT_Loader.src = 'https://frontesque.github.io/NEXT-UI-Framework/latest.js';
+    d.getElementsByTagName('head')[0].appendChild(NUXT_Loader);
+}(document));
