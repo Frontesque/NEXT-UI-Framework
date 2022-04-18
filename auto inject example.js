9876bf90ea689ebf5e0b22b3a1f4c44e00156cf1
Kenny <27463495+Frontesque@users.noreply.github.com>
2022-04-18 09:27:29 -0400

    Update auto inject example.js

--- auto inject example.js
+++ auto inject example.js
@@ -1,10 +1,10 @@
 (function(document, s) {
-    let NUXT_Loader = document.createElement('script');
-    NUXT_Loader.type = 'text/javascript';
-    NUXT_Loader.src = 'https://frontesque.github.io/NEXT-UI-Framework/latest.js';
-    document.getElementsByTagName('head')[0].appendChild(NUXT_Loader);
+    let NEXT_Loader = document.createElement('script');
+    NEXT_Loader.type = 'text/javascript';
+    NEXT_Loader.src = 'https://frontesque.github.io/NEXT-UI-Framework/latest.js';
+    document.getElementsByTagName('head')[0].appendChild(NEXT_Loader);
 
-    NUXT_Loader.onload = function(){
+    NEXT_Loader.onload = function(){
         // NEXT is injected here
     };
 }(document));
